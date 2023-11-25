8df84ce929c14fb71ab82f829e37d19d772c4ab3
Surani Sooriyaarachchi <suranisooriyaarachchi99429@gmail.com>
2023-11-25 16:10:27 +0530

    Refactor styles

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,4 +1,3 @@
-import { IoIosArrowBack } from "react-icons/io";
 import '../css/dashboard.css';
 import ChatList from "./ChatList";
 import { useState } from "react";
@@ -21,9 +20,6 @@ const Dashboard = () => {
     return (
         <>
             <div className='bg-[#006399] py-4 sticky top-0 left-0 right-0 z-20'>
-                <button className='float-left px-3'>
-                    <IoIosArrowBack className='text-3xl font-semibold pt-1 text-white' />
-                </button>
                 <h1 className='text-2xl font-semibold pl-5 text-white'>ChatPal</h1>
             </div>
             <div className="w-full px-4 py-2">
